2d511fc96bae259f2c836153b4ee9662dda44f76
Stevan Little <stevan.little@iinteractive.com>
2012-05-08 12:56:36 -0400

    remove the types, they were not used correctly. However we leave the module cause I suspect it will come in handy

--- lib/Data/Riak/Types.pm
+++ lib/Data/Riak/Types.pm
@@ -3,19 +3,10 @@ package Data::Riak::Types;
 use strict;
 use warnings;
 
-use MooseX::Types::Moose qw/Str Int ArrayRef HashRef/;
-use MooseX::Types -declare => [ qw/HTTPMessage HTTPResponse HTTPRequest HTTPHeadersActionPack HTTPHeadersActionPackLinkList Riak RiakResult/ ];
+use Moose::Util::TypeConstraints;
 
-class_type 'HTTPMessage', { class => 'HTTP::Message' };
-class_type 'HTTPResponse', { class => 'HTTP::Response' };
-class_type 'HTTPRequest', { class => 'HTTP::Request' };
+# ...
 
-class_type 'HTTPHeadersActionPack', { class => 'HTTP::Headers::ActionPack' };
-class_type 'HTTPHeadersActionPackLinkList', { class => 'HTTP::Headers::ActionPack::LinkList' };
-
-class_type 'Riak', { class => 'Data::Riak::HTTP' };
-class_type 'RiakResult', { class => 'Data::Riak::HTTP::Result' };
-
-1;
+no Moose::Util::TypeConstraints; 1;
 
 __END__
