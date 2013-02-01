e5acb612af77760e0306b8f59c22b613dac52cc4
Florian Ragwitz <rafl@debian.org>
2013-02-01 18:03:37 -0500

    Make etag and last_modified optional
    With older versions of Riak, these headers wouldn't always be returned.

--- lib/Data/Riak/Result/Object.pm
+++ lib/Data/Riak/Result/Object.pm
@@ -9,15 +9,15 @@ with 'Data::Riak::Result::WithLocation',
      'Data::Riak::Result::WithVClock';
 
 has etag => (
-    is       => 'ro',
-    isa      => 'Str',
-    required => 1,
+    is        => 'ro',
+    isa       => 'Str',
+    predicate => 'has_etag',
 );
 
 has last_modified => (
-    is       => 'ro',
-    isa      => 'HTTP::Headers::ActionPack::DateHeader',
-    required => 1,
+    is        => 'ro',
+    isa       => 'HTTP::Headers::ActionPack::DateHeader',
+    predicate => 'has_last_modified',
 );
 
 __PACKAGE__->meta->make_immutable;
