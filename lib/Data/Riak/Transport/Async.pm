74935f4c3a58552eb090809c2b96de9ff9ac0c24
Florian Ragwitz <rafl@debian.org>
2013-02-15 08:52:09 -0500

    Add tag roles for sync and async transports

---
+++ lib/Data/Riak/Transport/Async.pm
@@ -0,0 +1,8 @@
+package Data::Riak::Transport::Async;
+
+use Moose::Role;
+use namespace::autoclean;
+
+with 'Data::Riak::Transport';
+
+1;
---
+++ lib/Data/Riak/Transport/Sync.pm
@@ -0,0 +1,8 @@
+package Data::Riak::Transport::Sync;
+
+use Moose::Role;
+use namespace::autoclean;
+
+with 'Data::Riak::Transport';
+
+1;
